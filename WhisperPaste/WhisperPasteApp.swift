a791ea1c509acff8c5bf5d8e930cabbfd4522e47
Lee Bartelme <leeish@gmail.com>
2026-02-27 20:42:20 -0700

    Initial Commit

---
+++ WhisperPaste/WhisperPasteApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhisperPasteApp.swift
+//  WhisperPaste
+//
+//  Created by Lee Bartelme on 2/27/26.
+//
+
+import SwiftUI
+
+@main
+struct WhisperPasteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
